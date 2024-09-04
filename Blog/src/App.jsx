af164c1ff7de456f7c758159e24a710c4169f876
Aman Pandey <hello.mr.aman123@gmail.com>
2024-09-04 18:35:34 +0530

    Blog project completion

---
+++ Blog/src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import './App.css';
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index";
+import {Outlet} from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]); 
+
+  if (!loading) {
+    return (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header />
+          <main>
+            TODO: <Outlet /> 
+          </main>
+          <Footer />
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default App;
